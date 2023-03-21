cc8ccfdb5ade417d647d34aa249890d66f388114
Marvyn Kiplagat <marvynrogony@gmail.com>
2023-03-21 11:44:00 +0300

    student grade

--- stdgrade.js
+++ stdgrade.js
@@ -1,1 +1,19 @@
 // student grade generator
+function inputMarks (){
+    const grades = parseFloat(prompt("Enter grade: "));
+
+    if (grades > 79){
+        return "A";
+    } else if (grades >= 60 && grades <= 79){
+        return "B";
+    } else if(grades >= 49 && grades <= 59){
+        return "C";
+    } else if (grades >= 40 && grades <= 49){
+        return "D";
+    } else{
+        return "E";
+    }
+}
+
+const yourGrade = inputMarks();
+alert (`Your grade is ${yourGrade}`)
